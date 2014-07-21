d0cab662f2f986fcfc786d357959ef29be96652b
AutoBotG <Girish.Pottigari@uk.tesco.com>
2014-07-21 09:54:19 +0100

    catching Access Denied on child process so that the server starts

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,12 +12,29 @@ require 'browsermob/proxy'
 require 'browsermob/proxy/webdriver_listener'
 require 'json'
 require 'httparty'
+require 'rake'
 require File.dirname(__FILE__) +  '/tesco_browser_mob'
 require File.dirname(__FILE__) +  '/store_hars'
 
 
 server = BrowserMob::Proxy::Server.new(File.dirname(__FILE__) + "/../../resources/browsermob-proxy-2.0-beta-9/bin/browsermob-proxy.bat",:port => 9090, :log => true) #=> #<BrowserMob::Proxy::Server:0x000001022c6ea8 ...>
-server.start
+
+command = "FOR /F \"tokens=5 delims= \" %P IN ('netstat -a -n -o ^| findstr :9090.*0.0.0.0.*LISTENING') DO TaskKill.exe /F /PID %P"
+sh "#{command}" do |ok, result|
+  # Empty block so features continue to run if not ok
+end
+
+begin
+  server.start
+rescue Exception => e
+  Selenium::WebDriver::Wait.new(:timeout => 20).until do
+    begin
+      HTTParty.get("http://localhost:9090/proxy").success?
+    rescue
+    end
+  end
+end
+
 proxy = server.create_proxy
 proxy_listener =  BrowserMob::Proxy::WebDriverListener.new(proxy)
 
--- features/support/store_hars.rb
+++ features/support/store_hars.rb
@@ -1,12 +1,12 @@
-require 'rubygems'
-require 'mongo'
-
-include Mongo
-
-host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
-port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
-
-puts "Connecting to #{host}:#{port}"
-db = MongoClient.new(host, port).db('harstorage')
-@hars_collection = db['results']
-
+#require 'rubygems'
+#require 'mongo'
+#
+#include Mongo
+#
+#host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
+#port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
+#
+#puts "Connecting to #{host}:#{port}"
+#db = MongoClient.new(host, port).db('harstorage')
+#@hars_collection = db['results']
+#
